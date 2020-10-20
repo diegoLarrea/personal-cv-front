36117d84433a3a1f9f57c73d7d9e712291df6e48
Diego <diego.larrea@personal.com.py>
2020-10-20 09:18:35 -0300

    Base template y rutas

--- src/app/_pages/pages.component.ts
+++ src/app/_pages/pages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -12,4 +12,15 @@ export class PagesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  f1() {
+    $('#sidebar').removeClass('active');
+    $('.overlay').removeClass('active');
+  }
+
+  f2() {
+    $('#sidebar').addClass('active');
+    $('.overlay').addClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  }
 }
